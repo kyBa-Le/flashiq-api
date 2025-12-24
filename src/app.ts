98d5666dee647b9dfff7585eca081795ae496fd8
kyBa-Le <lekyba2000hc@gmail.com>
2025-12-24 16:34:56 +0700

    chore(test): add the full cors

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,15 @@ import routes from './routes';
 import dotenv from 'dotenv';
 import { apiLimiter } from './middlewares/rateLimiter.middleware';
 import { specs } from './docs/swagger';
+import cors from 'cors';
 
 const app = express();
 
 dotenv.config();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
