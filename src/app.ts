c344f199224d0f23d19d9b4a3ed87973d7effc9c
kyBa-Le <lekyba2000hc@gmail.com>
2025-12-24 17:22:00 +0700

    chore(project): allow cors for frontend

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,15 @@ import routes from './routes';
 import dotenv from 'dotenv';
 import { apiLimiter } from './middlewares/rateLimiter.middleware';
 import { specs } from './docs/swagger';
+import { corsConfig } from './config/cors';
 
 const app = express();
 
 dotenv.config();
 
 app.use(express.json());
+
+app.use(corsConfig());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
