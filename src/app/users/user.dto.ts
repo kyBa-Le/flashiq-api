a0ec5d3c080994cbe3710af134d0b00b5c52e729
kyBa-Le <lekyba2000hc@gmail.com>
2025-12-26 15:23:35 +0700

    feat(user): add the api get the current user

---
+++ src/app/users/user.dto.ts
@@ -0,0 +1,6 @@
+export interface UserResponseDto {
+  id: string;
+  username: string;
+  email: string;
+  avatar?: string;
+}
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import { SetController } from '../app/sets/set.controller';
+import { getCurrentUser } from '../app/users/user.controller';
+import { authenticateAccessToken } from '../middlewares/auth.middleware';
 
 const router = Router();
 
 router.get('/:userId/sets', SetController.getSetByUser);
+router.get('/me', authenticateAccessToken, getCurrentUser);
 
 export default router;
