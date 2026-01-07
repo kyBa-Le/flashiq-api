85fd7326276fc75dda6e37ecfeefa33533761ead
Tran Cong Doan <congdoan0806@gmail.com>
2026-01-07 17:10:21 +0700

    fix(quiz): fix data response

--- src/app/quiz/quiz.type.ts
+++ src/app/quiz/quiz.type.ts
@@ -2,18 +2,9 @@ export class Quiz {
   constructor(
     readonly cardId: string,
     readonly term: string,
-    readonly choices: string[] | null,
-    readonly correctAnswer: string
-  ) {}
-}
-
-export class MultipleChoiceQuiz {
-  constructor(
-    readonly cardId: string,
-    readonly term: string,
     readonly example: string | null,
     readonly image_url: string | null,
-    readonly choices: string[],
+    readonly choices: string[] | null,
     readonly correctAnswer: string
   ) {}
 }
